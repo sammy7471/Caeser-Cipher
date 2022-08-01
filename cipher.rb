c845bf79977ed64c582712581fcd8e570f09fd7f
Samuel Solomon <sammygideon01@gmail.com>
2022-08-01 15:53:38 +0000

    Create a char method for the Caeser Cipher

--- cipher.rb
+++ cipher.rb
@@ -1,6 +1,19 @@
 def caeser_cipher(string, key)
-    string = string.downcase
-    str = string.each_byte {|s| puts s - key}   
+    letters = ("a".."z").to_a
+    c_Letters = ("A".."Z").to_a
+    new_String = ""
+    string = string.downcase 
+
+    string.each_char do |x|
+      if letters.include?(x)
+        number = letters.index(x) + key
+        if number > 26
+          number = number % 26
+        end
+        new_String += letters[number]
+      end
+    end
+    puts new_String
 end
 
-caeser_cipher("Word", 5)
+caeser_cipher("What a string!", 5)
